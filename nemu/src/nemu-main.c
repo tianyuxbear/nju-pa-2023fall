60231be0ac9f28a54bbddb38f0316f53ac50955e
tracer-ics2023 <tracer@njuics.org>
2023-12-05 18:56:48 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:56:48 up 8 min,  0 users,  load average: 0.04, 0.02, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -20,8 +20,6 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
-//#define CONFIG_TARGET_AM
-
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
