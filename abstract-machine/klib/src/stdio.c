041e782c26eae8968ced58a126b7660ba1ef5ed1
tracer-ics2023 <tracer@njuics.org>
2023-12-06 15:25:54 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:25:54 up  6:12,  0 users,  load average: 0.01, 0.05, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,88 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
+int handle_int(char* str, int num);
+
+int printf(const char *format, ...) {
   panic("Not implemented");
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+int sprintf(char *str, const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+
+  int nbyte = 0;
+  while(*format != '\0'){
+    if(*format == '%'){
+      format++;
+      switch(*format)
+      {
+        case 's': 
+          char* arg_str = va_arg(args, char*);
+          strncpy(str, arg_str, strlen(arg_str));
+          format++;
+          nbyte += strlen(arg_str);
+          break;
+        case 'd':
+          int arg_int = va_arg(args, int);
+          format++;
+          nbyte += handle_int(str, arg_int);
+          break;
+        default:
+          assert(0);
+          break;
+      }
+    }else{
+      *str++ = *format++;
+      nbyte++;
+    }
+  }
+  *str = '\0';
+  return nbyte;
+}
+
+int snprintf(char *str, size_t size, const char *format, ...) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
+int vsprintf(char *str, const char *format, va_list ap) {
   panic("Not implemented");
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
+int vsnprintf(char *str, size_t size, const char *format, va_list ap) {
   panic("Not implemented");
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+
+int handle_int(char* str, int num){
+  char num_str[15];
+  int index = 0;
+
+  int is_negative = 0;
+  if(num < 0){
+    is_negative = 1;
+    num = -num;
+  }
+
+  while(num != 0){
+    char byte = num % 10;
+    num_str[index++] = '0' + byte;
+    num /= 10;
+  }
+  if(index == 0) num_str[index++] = '0';
+  if(is_negative) num_str[index++] = '-';
+
+  for(int i = 0, j = index - 1; i < j; i++, j--){
+    char tmp = num_str[i];
+    num_str[i] = num_str[j];
+    num_str[j] = tmp;
+  }
+  num_str[index] = '\0';
+
+  int bytes = strlen(num_str);
+  strncpy(str, num_str,bytes);
+  
+  return bytes;
 }
 
 #endif
