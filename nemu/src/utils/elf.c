60faa70bae5405e4d8bbd460e387c08ad2120f9d
tracer-ics2023 <tracer@njuics.org>
2023-12-08 14:01:18 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:01:18 up  4:17,  0 users,  load average: 0.08, 0.06, 0.02

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -124,8 +124,7 @@ void init_elf(const char* elf_file){
 	free(section_headers);
 
 	for(int i = 0; i < sym_num; i++)
-		if(symtab[i].st_info == STT_FUNC) 
-			printf("find one function ==> value: 0x%016lx", symtab[i].st_value);
+		printf("value: 0x%016lx		type: %d\n", symtab[i].st_value, symtab[i].st_info);
 
 	return;
 }
