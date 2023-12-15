d5bc8dbda01960c08c45446995efaba257b5bb11
tracer-ics2023 <tracer@njuics.org>
2023-12-15 17:17:36 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:17:36 up  7:22,  0 users,  load average: 0.16, 0.36, 0.37

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -13,13 +13,12 @@ Context* __am_irq_handle(Context *c) {
         break;
       default: ev.event = EVENT_ERROR; break;
     }
-    // for(int i = 0; i < 32; i++){
-    //   printf("x%d ==> 0x%08x", i, c->gpr[i]);
-    // }
+    for(int i = 0; i < 32; i++){
+      printf("x%d ==> 0x%016x", i, c->gpr[i]);
+    }
     printf("mcause ==> 0x%016x\n", c->mcause); 
     printf("mstatus ==> 0x%016x\n", c->mstatus);
     printf("mepc ==> 0x%016x\n", c->mepc);
-    printf("hello world\n");
     c = user_handler(ev, c);
     assert(c != NULL);
   }
