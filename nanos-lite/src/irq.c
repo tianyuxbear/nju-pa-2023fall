07cdc93e437f49935e66709e4e493c980e73a9ab
tracer-ics2023 <tracer@njuics.org>
2023-12-17 13:53:23 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:53:23 up 38 min,  0 users,  load average: 0.09, 0.15, 0.10

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,12 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    default: panic("Unhandled event ID = %d", e.event);
+    case EVENT_YIELD:
+      printf("=== Trigger yield event ===\n");
+      break;
+    default: 
+      panic("Unhandled event ID = %d", e.event);
+      break;
   }
 
   return c;
