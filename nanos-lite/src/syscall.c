57489fc51a82e14b6de9be953b16071c89936cda
tracer-ics2023 <tracer@njuics.org>
2023-12-17 19:37:49 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:37:49 up  6:23,  0 users,  load average: 0.10, 0.33, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -29,8 +29,8 @@ void do_syscall(Context *c) {
   uint64_t a[4];
   a[0] = c->GPR1;  //a7
   a[1] = c->GPR2;  //a0
-  a[2] = c->GPR2;  //a1
-  a[3] = c->GPR2;  //a2
+  a[2] = c->GPR3;  //a1
+  a[3] = c->GPR4;  //a2
 
 
   switch (a[0]) {
@@ -50,7 +50,13 @@ void do_syscall(Context *c) {
       break;
     case SYS_write:
       printf("=== syscall: %s ===\n", syscall_name[SYS_write]);
-      printf("fd: 0x%016x\n buf: 0x%016x\n count: 0x%016x\n", a[1], a[2], a[3]);
+      int fd = (int)a[1];
+      char* buf = (char*) a[2];
+      size_t count = (uint32_t) a[3];
+      if(fd == 1 || fd == 2){
+        for(int i = 0; i < count; i++) putch(buf[i]);
+        c->GPR2 = count;
+      } 
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
