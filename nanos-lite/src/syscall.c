0a2947157ef062cde2797ec3d14c5a2a4f6cab4d
tracer-ics2023 <tracer@njuics.org>
2023-12-17 18:38:35 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:38:35 up  5:23,  0 users,  load average: 0.72, 0.47, 0.30

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,17 +1,42 @@
 #include <common.h>
 #include "syscall.h"
+
+static char* syscall_name[] = {
+  "SYS_exit",
+  "SYS_yield",
+  "SYS_open",
+  "SYS_read",
+  "SYS_write",
+  "SYS_kill",
+  "SYS_getpid",
+  "SYS_close",
+  "SYS_lseek",
+  "SYS_brk",
+  "SYS_fstat",
+  "SYS_time",
+  "SYS_signal",
+  "SYS_execve",
+  "SYS_fork",
+  "SYS_link",
+  "SYS_unlink",
+  "SYS_wait",
+  "SYS_times",
+  "SYS_gettimeofday"
+};
+
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
     case SYS_exit:
-      printf("=== SYS_call: exit ===\n");
+      printf("=== syscall: %s ===\n", syscall_name[SYS_exit]);
       a[1] = c->GPR2;
       halt(a[1]);
       break;
     case SYS_yield:
-      printf("=== SYS_call: yield ===\n");
+      printf("=== syscall: %s ===\n", syscall_name[SYS_yield]);
       yield();
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
