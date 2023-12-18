032ebf9cabc1bafb1319636ebba9313a9bea55e2
tracer-ics2023 <tracer@njuics.org>
2023-12-18 13:51:44 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:51:44 up  4:08,  0 users,  load average: 0.02, 0.03, 0.04

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,16 +2,18 @@
 #include <stdio.h>
 
 int main() {
-  write(1, "Hello World!\n", 13);
-  int i = 1;
-  volatile int j = 0;
-  while (1) {
-    j ++;
-    if (j == 10000) {
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      j = 0;
-    }
-    if(i == 5) break;
-  }
+  // write(1, "Hello World!\n", 13);
+  // int i = 1;
+  // volatile int j = 0;
+  // while (1) {
+  //   j ++;
+  //   if (j == 10000) {
+  //     printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+  //     j = 0;
+  //   }
+  //   if(i == 5) break;
+  // }
+  printf("Hello World!\n");
+
   return 0;
 }
