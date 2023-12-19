764208ff3f42f91dfb718c7f1b5223a3bb3a2899
tracer-ics2023 <tracer@njuics.org>
2023-12-19 20:20:32 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:20:32 up  5:59,  0 users,  load average: 0.05, 0.12, 0.15

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -20,10 +20,12 @@ void hello_fun(void *arg) {
 }
 
 Context *context_kload(PCB* kpcb, void (*entry)(void *), void *arg) {
-  Context *cp = kcontext((Area) { kpcb->stack, kpcb + 1 }, entry, arg);
-  kpcb->cp = cp;
+  Area stack;
+  stack.start = kpcb->stack;
+  stack.end = kpcb->stack + STACK_SIZE;
+  kpcb->cp = kcontext(stack, entry, arg);
   printf("kpcb: 0x%016x     kpcb->cp: 0x%016x\n", kpcb, kpcb->cp);
-  return cp;
+  return kpcb->cp;
 }
 
 
