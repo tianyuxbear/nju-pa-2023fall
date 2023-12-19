9f890c1046603e05b12e2319c87d66d8c7649823
tracer-ics2023 <tracer@njuics.org>
2023-12-19 10:27:54 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:27:54 up 16 min,  0 users,  load average: 0.37, 0.25, 0.14

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
 
+extern uint32_t NDL_Init();
+extern uint32_t NDL_Quit();
 extern uint32_t NDL_GetTicks();
 
 int main()
 {
 	struct timeval tv;
 	uint32_t ms = 500;
+	NDL_Init();
 	uint32_t ticks = NDL_GetTicks();
 	while(1){
 		while(ticks < ms){
@@ -14,5 +17,8 @@ int main()
 		}
 		printf("ms = %d\n", (int)ms);
 		ms += 500;
+		if(ms == 10000) break;
 	}
+	NDL_Quit();
+	return 0;
 }
