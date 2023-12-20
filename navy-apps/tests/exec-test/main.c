f54fe680e005ba381d22cb4a449afe93cfecfc55
tracer-ics2023 <tracer@njuics.org>
2023-12-20 19:42:51 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:42:51 up 10:02,  0 users,  load average: 0.27, 0.24, 0.19

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[], char* envp[]) {
   int n = 1;
   char buf[16];
   sprintf(buf, "%d", n + 1);
+  printf("argv[0]: %p    buf: %p\n", (uint64_t)argv[0], (uint64_t)buf);
   execl(argv[0], argv[0], buf, NULL);
   return 0;
 }
