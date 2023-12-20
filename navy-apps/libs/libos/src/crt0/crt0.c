8a293a5278c3ce7cff8c27dcbef001ea1fe06934
tracer-ics2023 <tracer@njuics.org>
2023-12-20 18:04:44 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:04:44 up  8:24,  0 users,  load average: 0.12, 0.14, 0.15

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,30 +5,31 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  uint64_t addr = (uint64_t)args;
-  int argc = *((int*)addr);
-  addr += 8;
-  uint64_t str_area = addr;
-  int i = 0;
-  while(i != 2){
-    if(*((uint64_t*)str_area) == 0) i++;
-    str_area += 8;
-  }
+  // uint64_t addr = (uint64_t)args;
+  // int argc = *((int*)addr);
+  // addr += 8;
+  // uint64_t str_area = addr;
+  // int i = 0;
+  // while(i != 2){
+  //   if(*((uint64_t*)str_area) == 0) i++;
+  //   str_area += 8;
+  // }
 
-  char argv[argc][32];
-  memset(argv, 0, sizeof(argv));
-  for(int i = 0; i < argc; i++){
-    memcpy(argv[i], (void*)(str_area + i * 32), 32);
-  }
+  // char argv[argc][32];
+  // memset(argv, 0, sizeof(argv));
+  // for(int i = 0; i < argc; i++){
+  //   memcpy(argv[i], (void*)(str_area + i * 32), 32);
+  // }
 
-  int envc = (str_area - addr) / 8 - 1 - 1 - argc;
-  str_area += argc * 32;
-  char envp[envc][32];
-  memset(envp, 0, sizeof(envp));
-  for(int i = 0; i < envc; i++){
-    memcpy(envp[i], (void*)(str_area + i * 32), 32);
-  }
+  // int envc = (str_area - addr) / 8 - 1 - 1 - argc;
+  // str_area += argc * 32;
+  // char envp[envc][32];
+  // memset(envp, 0, sizeof(envp));
+  // for(int i = 0; i < envc; i++){
+  //   memcpy(envp[i], (void*)(str_area + i * 32), 32);
+  // }
 
-  exit(main(argc, (char**)argv, (char**)envp));
+  //exit(main(argc, (char**)argv, (char**)envp));
+  exit(main(0, NULL, NULL));
   assert(0);
 }
