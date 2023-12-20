176882b718bf50f5d54a256c0b16ecea0054639a
tracer-ics2023 <tracer@njuics.org>
2023-12-20 18:21:09 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:21:09 up  8:40,  0 users,  load average: 0.17, 0.22, 0.20

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -3,7 +3,9 @@
 
 int main(int argc, char *argv[], char *envp[]) {
   printf("main ==> argc: %d    argv: 0x%016x    envp: 0x%016x\n", argc, (uint64_t)argv, (uint64_t)envp);
-  printf("1st arg: %s\n", (char*)argv);
+  for(int i = 0; i < argc; i++){
+    printf("%d arg ==> %s\n", i + 1, (char*)((uint64_t)argv + i * 32));
+  }
   write(1, "Hello World!\n", 13);
   int i = 1;
   volatile int j = 0;
