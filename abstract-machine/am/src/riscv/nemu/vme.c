ddf6611b0a3283a94d341474b881386039c36f09
tracer-ics2023 <tracer@njuics.org>
2023-12-25 18:18:00 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:18:00 up  8:18,  0 users,  load average: 0.06, 0.10, 0.10

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -23,8 +23,8 @@ static Area segments[] = {      // Kernel memory mappings
 
 
 static inline void set_satp(void *pdir) {
-  printf("enter set_satp\n");
   uint64_t mode = 1ul << (__riscv_xlen - 1);
+  printf("enter set_satp\n");
   asm volatile("csrw satp, %0" : : "r"(mode | ((uint64_t)pdir >> 12)));
   printf("leave set_satp\n");
 }
