b19cd5b6a2f9f557d15a5bcaad014a45bb54a525
tracer-ics2023 <tracer@njuics.org>
2023-12-25 17:19:41 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:19:41 up  7:20,  0 users,  load average: 0.16, 0.15, 0.14

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -39,10 +39,8 @@
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) { 
   assert(PGROUNDDOWN(vaddr) == PGROUNDDOWN(vaddr + len));
   uint64_t offset = vaddr & 0x0fff;
-  printf("satp: 0x%016lx\n", cpu.csr.satp << 12);
-  printf("*satp: 0x%016lx\n", *(uint64_t*)(cpu.csr.satp << 12));
   uint64_t* root = (uint64_t*)(cpu.csr.satp << 12);
-  printf("*root: 0x%016lx\n", *root);
+  printf("root: %p\n", root);
   
   uint32_t va_vpn2 = VA_VPN2(vaddr);
   uint32_t va_vpn1 = VA_VPN1(vaddr);
