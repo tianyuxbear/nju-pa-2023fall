0649b27f3e43175a5ab54ad913690d4af82d14c2
tracer-ics2023 <tracer@njuics.org>
2023-12-26 16:40:43 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:40:43 up  6:08,  0 users,  load average: 0.29, 0.22, 0.14

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -57,6 +57,7 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
+  printf("map_read\n");
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
@@ -69,6 +70,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
+  printf("map_write\n");
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
