260fad16b6493c73f60918c9f3c298744af58e60
tracer-ics2023 <tracer@njuics.org>
2023-12-26 12:39:38 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:39:38 up  2:07,  0 users,  load average: 0.16, 0.07, 0.02

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -29,17 +29,16 @@
 
 // translation use
 #define OFFSET(addr) (addr & 0x0fff)
-#define VA_VPN0(va) ((va >> 12) & 0x01ff)
-#define VA_VPN1(va) ((va >> 21) & 0x01ff)
-#define VA_VPN2(va) ((va >> 30) & 0x01ff) 
-#define PA2PTE(addr) ((addr >> 12) << 10)
-#define PTE2PA(pte) ((pte >> 10) << 12)
+#define VA_VPN0(va) ((va >> 12) & 0x00000000000001ff)
+#define VA_VPN1(va) ((va >> 21) & 0x00000000000001ff)
+#define VA_VPN2(va) ((va >> 30) & 0x00000000000001ff) 
+#define PA2PTE(addr) (((addr >> 12) & 0xfffffffffff) << 10)
+#define PTE2PA(pte) (((pte >> 10) & 0xfffffffffff) << 12)
 #define PGROUNDDOWN(addr) ((addr) & (~((1ull << 12) - 1)))
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) { 
-  printf("Enter translate!\n");
   assert(PGROUNDDOWN(vaddr) == PGROUNDDOWN(vaddr + len));
-  uint64_t offset = vaddr & 0x0fff;
+  uint64_t offset = vaddr & 0x0000000000000fff;
   uint64_t root = cpu.csr.satp << 12;
   
   uint32_t va_vpn2 = VA_VPN2(vaddr);
