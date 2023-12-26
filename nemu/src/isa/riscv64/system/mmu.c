2d207229c4a06e6ec998d07c414d1139ca254d05
tracer-ics2023 <tracer@njuics.org>
2023-12-26 12:48:23 +0800

    >  compile NEMU 2020080901005 熊天宇 Linux ubuntu 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:48:23 up  2:15,  0 users,  load average: 0.14, 0.12, 0.06

--- nemu/src/isa/riscv64/system/mmu.c
+++ nemu/src/isa/riscv64/system/mmu.c
@@ -38,7 +38,7 @@
 
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) { 
   printf("vaddr: 0x%lx  len: %d   vaddr + len: 0x%lx\n", vaddr, len, vaddr + len);
-  assert(PGROUNDDOWN(vaddr) == PGROUNDDOWN(vaddr + len));
+  assert(PGROUNDDOWN(vaddr) == PGROUNDDOWN(vaddr + len - 1));
   uint64_t offset = vaddr & 0x0000000000000fff;
   uint64_t root = cpu.csr.satp << 12;
   
